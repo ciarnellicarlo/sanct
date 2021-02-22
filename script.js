d6bb6694d30a8311594fbcac39487072ec92fe4c
Carlo Ciarnelli <ciarnellicarloedoardo@gmail.com>
2021-02-22 13:39:06 +0100

    Seventh commit, hamburger menu style and animation change, h2 margin change

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const buttonsOpen = document.querySelectorAll('.click-me');
 const buttonsClose = document.querySelectorAll('.close-modal');
 const closeModal = document.querySelectorAll('.modal-background');
+const mobileMenu = document.querySelector('.burger-menu');
+const mobileNav = document.querySelector('.menu');
+const mobileStrips = document.querySelectorAll('.strip');
+const menu = document.querySelectorAll('.menu li');
+
+//Modal animation
 
 buttonsOpen.forEach(item => item.addEventListener('click', e => e.currentTarget.parentElement.nextElementSibling.classList.add('show')));
 
@@ -8,10 +14,29 @@ buttonsClose.forEach(item => item.addEventListener('click', e => e.currentTarget
 
 closeModal.forEach(item => item.addEventListener('click', e => e.currentTarget.closest('.modal-container').classList.remove('show')));
 
+//Mobile menu animation
+
+const menuSlide = () => {
+    mobileMenu.addEventListener('click',() => {
+        mobileNav.classList.toggle('mobile-menu-active');
+        mobileStrips.forEach(item => item.classList.toggle('strip-active'));
+        menu.forEach((item, index) => {
+            if (item.style.animation) {
+                item.style.animation = ''
+            } else {
+                item.style.animation = `menuFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+            }
+        });
+    });
+    
+};
+
+menuSlide();
+
 AOS.init()
 AOS.init({
     disable: function() {
-        var maxWidth = 800;
+        var maxWidth = 768;
         return window.innerWidth < maxWidth;
       }
 })
